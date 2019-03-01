a536724fb1a762119562703ca9ef26205068368e
Giang Nguyen <thuygiang152@gmail.com>
2019-03-01 18:45:17 +0200

    second commit

---
+++ src/Projects.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PROJECTS from './data/project';
+
+class Project extends Component {
+    render() {
+        console.log('this.props', this.props);
+        const { title, image, description, link } = this.props.project;
+        return (
+            <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
+                <h3 >{title}</h3>
+                <img src={image} alt='profile' style={{ width: 200, height: 120 }} />
+                <p>{description}</p>
+                <a href={link} style={{ fontSize: 15 }}>{link}</a>
+            </div >
+        )
+    }
+}
+
+class Projects extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Highlighted Projects (NOT my Projects hihi)</h2>
+                <div>
+                    {
+                        PROJECTS.map((project, index) => {
+                            return (
+                                <Project key={index} project={project} />
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Projects;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,31 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+
+const SOCIAL_PROFILES = [
+    {
+        name: 'Email',
+        link: 'mailto: thuygiang152@gmail.com',
+        image: emailIcon
+    },
+    {
+        name: 'GitHub',
+        link: 'http://github.com/thuygiang152',
+        image: githubIcon
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/giangnguyenthuy/',
+        image: linkedinIcon
+    },
+    {
+        name: 'Twitter',
+        link: 'https://twitter.com/thuygiang152',
+        image: twitterIcon
+    }
+];
+
+export default SOCIAL_PROFILES;
+
